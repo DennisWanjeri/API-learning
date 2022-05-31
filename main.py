062c4843dbb81e01f8c54b6a54c13ca2f37dce39
Dennis Wanjeri <77827277+DennisWanjeri@users.noreply.github.com>
2022-05-31 17:55:59 +0200

    created a fast api

---
+++ main.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+app = FastAPI()
+
+
+class posts(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "post title", "content": "post content", "id": 1}, {
+    "title": "favourite foods", "content": "mukimo ya waru", "id": 2}]
+
+
+def find_posts(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
+def find_index_posts(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
+@app.get("/")
+def root():
+    return {"message": "welcome to my api!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: posts):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_posts(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
+    return {"post detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_posts(id: int):
+    index = find_index_posts(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
